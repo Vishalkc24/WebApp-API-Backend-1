cdd98e35e9e20768828e84454f8557828e37f77a
Vishalkc24 <vishalkc2402@gmail.com>
2025-02-18 15:44:15 +0530

    Added a route to include getTripByID in the tripRoutes.js file

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -11,5 +11,8 @@ const tripController = require('../controller/tripController');
 // This route listens for GET requests on '/api/trips' and calls getAllTrips function
 router.get('/api/trips', tripController.getAllTrips);
 
+// Define the route to get a specific trip by trip_id
+router.get('/api/trips/:trip_id', tripController.getTripByID);
+
 // Export the router to be used in the main application
 module.exports = router;
